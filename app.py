06fe1b709c6446e1e6a0e6dd38934509739ef62e
Mahesh Kumar <maheshkumar.wdp21@gmail.com>
2015-08-02 21:11:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, url_for, render_template, redirect
+from flask import Flask, request
+from flask import url_for, render_template
+from flask import redirect
 
 app = Flask(__name__)
 
@@ -6,8 +8,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template("index.html")
-
-
-
+	
 if __name__ == "__main__":
     app.run(debug=True)
